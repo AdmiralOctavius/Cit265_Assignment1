2b527a377ac95e4a219c0a9d5a7d40f2feace918
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-08-27 12:48:16 -0400

    Start of Assignment 1

---
+++ Cit265_Assignment1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cit265_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Line
+    {
+        private double a, b, c;
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        //Six different methods
+        double ReturnX(double y)
+        {
+            double x = 0;
+
+
+
+            return x;
+        }
+    }
+}
