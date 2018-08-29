cc5450abc6f322b6eaff2bf5b7a52169e883ce1e
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-08-29 12:56:28 -0400

    End of Class 29
    Almost done

---
+++ Cit265_Assignment1/Line.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cit265_Assignment1
+{
+    class Line
+    {
+        private double a, b, c;
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public bool isValid()
+        {
+            if(A != 0 || B != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Six different methods
+        public double ReturnX(double y)
+        {
+ 
+                double tempB = y * B;
+                double tempC = C - tempB;
+                double x = tempC / A;
+                return x;
+
+        }
+
+        public double ReturnY(double x)
+        {
+            double tempA = x * A;
+            double tempC = C - tempA;
+            double y = tempC / B;
+            return y;
+        }
+
+        public double ReturnSlope()
+        {
+            double slope = 0;
+            if (B != 0)
+            {
+                slope = (-1 * A) / B;
+            }
+            return slope;
+        }
+
+        public bool CompareParallel(Line lineA)
+        {
+            bool areParallel = false;
+
+            if(lineA.B ==0 && B == 0)
+            {
+                areParallel = true;
+            }
+            else if(lineA.ReturnSlope() == ReturnSlope())
+            {
+                areParallel = true;
+            }
+            else
+            {
+
+            }
+
+            return areParallel;
+        }
+        
+        public bool ComparePerpindicular(Line lineA)
+        {
+            bool arePerpindicular = false;
+
+            if(lineA.ReturnSlope() * this.ReturnSlope() == -1)
+            {
+                arePerpindicular = true;
+            }
+            else if(lineA.B == 0 && this.A == 0 || this.B == 0 && lineA.A == 0)
+            {
+                arePerpindicular = true;
+            }
+            else
+            {
+
+            }
+                return arePerpindicular;
+
+        }
+        /*
+        void CompareIntersect(Line lineA, Line lineB)
+        {
+
+        }*/
+    }
+}
--- Cit265_Assignment1/Program.cs
+++ Cit265_Assignment1/Program.cs
@@ -10,71 +10,25 @@ namespace Cit265_Assignment1
     {
         static void Main(string[] args)
         {
+            Line myLine = new Line();
+            myLine.A =0;
+            myLine.B = 0;
+            myLine.C = 67;
+            Console.WriteLine(myLine.isValid());
+            //Console.WriteLine(myLine.A);
+            Console.WriteLine(myLine.ReturnX(3));
+            Console.WriteLine(myLine.ReturnY(11));
+
+            Console.WriteLine(myLine.ReturnSlope());
+
+            Line myLineB = new Line();
+            myLineB.A = 34;
+            myLineB.B = 0;
+            myLineB.C = 101;
+            Console.WriteLine(myLine.CompareParallel(myLineB));
+            Console.WriteLine(myLine.ComparePerpindicular(myLineB));
         }
     }
 
-    class Line
-    {
-        private double a, b, c;
-        public double A
-        {
-            get { return a; }
-            set { a = value; }
-        }
-        public double B
-        {
-            get { return b; }
-            set { b = value; }
-        }
-        public double C
-        {
-            get { return c; }
-            set { c = value; }
-        }
-
-        //Six different methods
-        double ReturnX(double y)
-        {
-
-            double tempB = y * B;
-            double tempC = C - tempB;
-            double x = tempC / A;
-            return x;
-        }
-
-        double ReturnY(double x)
-        {
-            double tempA = x * A;
-            double tempC = C - tempA;
-            double y = tempC / B;
-            return y;
-        }
-
-        double ReturnSlope()
-        {
-            double slope = 0;
-            if (b != 0)
-            {
-                slope = (-1 * a) / b;
-            }
-            return slope;
-        }
-
-        bool CompareParallel(Line lineA, Line lineB)
-        {
-
-
-        }
-
-        bool ComparePerpindicular(Line lineA, Line lineB)
-        {
-
-
-        }
-
-        void CompareIntersect(Line lineA, Line lineB)
-        {
-
-        }
-    }
+    
 }
